0f10d4d14590e037d63e50c884de20795fcb98a9
svivanov <ivanov161@gmail.com>
2022-04-08 22:43:06 +0300

    Interaction with line shapes and threshold

--- src/Tests/LineOptimization/LineOptimizationTest.cpp
+++ src/Tests/LineOptimization/LineOptimizationTest.cpp
@@ -5,8 +5,6 @@
 
 #include <vector>
 
-#define THRESHOLD 0.3f
-
 LineOptimizationTest::LineOptimizationTest(Director& director, const std::string& name)
    : Test(director, name)
 {
@@ -16,47 +14,76 @@ LineOptimizationTest::LineOptimizationTest(Director& director, const std::string
 LineOptimizationTest::tPolyLine LineOptimizationTest::createRandomPolyLine()
 {
    tPolyLine result;
-   #if 0
-      int count = 50;
-      for(int i = 0; i < count; i++)
+   switch(mShape)
+   {
+      case SHAPE0:
       {
-         /*if(i == 25)
+         int count = 50;
+         for(int i = 0; i < count; i++)
          {
-            result.push_back(Vector2f(i / float(count) * 0.8f + 0.1f, 1.0f));
-         }
-         else*/
-         {
-            result.push_back(Vector2f(i / float(count) * 0.8f + 0.1f, sin(i / 5.0f) * 0.5f + 0.5f));
+            /*if(i == 25)
+            {
+               result.push_back(Vector2f(i / float(count) * 0.8f + 0.1f, 1.0f));
+            }
+            else*/
+            {
+               result.push_back(Vector2f(i / float(count) * 0.8f + 0.1f, sin(i / 5.0f) * 0.5f + 0.5f));
+            }
          }
       }
-   #else
-      #if 0
+      break;
+      case SHAPE1:
+      {
          result.push_back(Vector2f(0.1f, 0.1f));
          result.push_back(Vector2f(0.5f, 0.5f));
          result.push_back(Vector2f(0.9f, 0.1f));
-      #else
+      }
+      break;
+      case SHAPE2:
+      {
+         result.push_back(Vector2f(0.1f, 0.1f));
+         result.push_back(Vector2f(0.9f, 0.1f));
+      }
+      break;
+      case SHAPE3:
+      {
          result.push_back(Vector2f(0.1f, 0.1f));
          result.push_back(Vector2f(0.2f, 0.1f));
          result.push_back(Vector2f(0.3f, 0.1f));
          result.push_back(Vector2f(0.4f, 0.7f));
          result.push_back(Vector2f(0.6f, 0.2f));
-         result.push_back(Vector2f(0.9f, 0.1f));
+         result.push_back(Vector2f(0.1f, 0.5f));
          result.push_back(Vector2f(0.9f, 0.9f));
          result.push_back(Vector2f(0.1f, 0.9f));
-      #endif
-   #endif
+      }
+      break;
+   }
 
    return result;
 }
 
 void LineOptimizationTest::createPolyLineVBO(GLuint& polyLineVBO, const tPolyLine& polyLine)
 {
-   glGenBuffers(1, &polyLineVBO);
-   checkGLError("Gen poly line vbo");
+   if(polyLineVBO == 0)
+   {
+      glGenBuffers(1, &polyLineVBO);
+      checkGLError("Gen poly line vbo");
+   }
    glBindBuffer(GL_ARRAY_BUFFER, polyLineVBO);
    checkGLError("Bind poly line vbo");
    glBufferData(GL_ARRAY_BUFFER, polyLine.size() * sizeof(Vector2f), &polyLine[0], GL_STATIC_DRAW);
    checkGLError("Pass poly line vertices to GPU");
+}
+
+void LineOptimizationTest::updateLines()
+{
+   //poly lines
+   mPolyLine = createRandomPolyLine();
+   mPolyLineOptimized = optimizePolyLine(mPolyLine, mThreshold);
+
+   //vbo
+   createPolyLineVBO(mPolyLineVBO, mPolyLine);
+   createPolyLineVBO(mPolyLineOptimizedVBO, mPolyLineOptimized);
 }
 
 void LineOptimizationTest::init()
@@ -65,13 +92,7 @@ void LineOptimizationTest::init()
    mPolyLineShader.load("Data/Shader/PolyLine/polyLine.vs", "Data/Shader/PolyLine/polyLine.fs");
    mPolyLineShader.bind();
 
-   //poly lines
-   mPolyLine = createRandomPolyLine();
-   mPolyLineOptimized = optimizePolyLine(mPolyLine, THRESHOLD);
-
-   //vbo
-   createPolyLineVBO(mPolyLineVBO, mPolyLine);
-   createPolyLineVBO(mPolyLineOptimizedVBO, mPolyLineOptimized);
+   updateLines();
 
    //states
    glDisable(GL_DEPTH_TEST);
@@ -94,51 +115,59 @@ LineOptimizationTest::tPolyLine LineOptimizationTest::optimizePolyLine(const tPo
    auto linePointCount = polyLine.size();
    if(linePointCount > 1)
    {
-      float prevLength = 0.0f;
-      for(auto pointIndex = 0u; pointIndex < linePointCount - 1; ++pointIndex)
+      if(linePointCount == 2u)
       {
-         const auto p0 = polyLine[pointIndex];
-         if(pointIndex == 0u)
+         result.push_back(polyLine[0]);
+         result.push_back(polyLine[1]);
+      }
+      else
+      {
+         float prevLength = 0.0f;
+         for(auto pointIndex = 0u; pointIndex < linePointCount - 1; ++pointIndex)
          {
-            result.push_back(p0);
-         }
-         const auto p1 = polyLine[pointIndex + 1];
-         if(linePointCount == 2u)
-         {
-            result.push_back(p1);
-         }
-         else
-         {
-            auto v = p1 - p0;
-            auto l = v.length();
-            
-            if(l > threshold && pointIndex > 0)
+            const auto p0 = polyLine[pointIndex];
+            if(pointIndex == 0u)
             {
                result.push_back(p0);
-               prevLength = 0.0;
             }
             else
             {
-               if(prevLength + l > threshold)
+               const auto p1 = polyLine[pointIndex + 1];
+               auto v = p1 - p0;
+               auto l = v.length();
+
+               if(l > threshold)
                {
-                  auto t = threshold - prevLength;
-                  auto newPoint = p0 + v * t;
-                  result.push_back(newPoint);
-                  prevLength = l - t;
+                  result.push_back(p0);
+                  prevLength = 0.0;
                }
                else
                {
-                  prevLength += l;
-               }
-               if(prevLength > threshold)
-               {
-                  prevLength = threshold;
-               }
-            }
+                  if(prevLength + l > threshold)
+                  {
+                     auto t = threshold - prevLength;
+                     auto vn = v;
+                     vn.normalize();
+                     auto newPoint = p0 + vn * t;
+                     //auto newPoint = (p0 + p1) * 0.5f;
+                     result.push_back(newPoint);
+                     prevLength = l - t;
+                  }
+                  else
+                  {
+                     prevLength += l;
+                  }
 
-            if(pointIndex == linePointCount - 2)
-            {
-               result.push_back(p1);
+                  /*if(prevLength > threshold)
+                  {
+                     prevLength = threshold;
+                  }*/
+               }
+
+               if(pointIndex == linePointCount - 2)
+               {
+                  result.push_back(p1);
+               }
             }
          }
       }
@@ -174,4 +203,30 @@ void LineOptimizationTest::run()
 {
    renderPolyLine(mPolyLineVBO, Vector4f(1.0f, 0.0f, 0.0f, 1.0f), mPolyLine.size(), 20.0f);
    renderPolyLine(mPolyLineOptimizedVBO, Vector4f(0.0f, 1.0f, 0.0f, 1.0f), mPolyLineOptimized.size(), 10.0f);
+}
+
+void LineOptimizationTest::onKeyboardEvent(KeyboardEventType eventType, int keyCode)
+{
+   if(eventType == KE_DOWN)
+   {
+      switch(keyCode)
+      {
+      case VK_LEFT:
+         mThreshold -= 0.01f;
+         updateLines();
+         break;
+      case VK_RIGHT:
+         mThreshold += 0.01f;
+         updateLines();
+         break;
+      case 'S':
+         mShape = (mShape + 1) % SHAPE_COUNT;
+         updateLines();
+         break;
+      case 's':
+         mShape = (mShape - 1) % SHAPE_COUNT;
+         updateLines();
+         break;
+      }
+   }
 }
--- src/Tests/LineOptimization/LineOptimizationTest.h
+++ src/Tests/LineOptimization/LineOptimizationTest.h
@@ -6,6 +6,15 @@
 
 #include <vector>
 
+enum EShapeType
+{
+   SHAPE0,
+   SHAPE1,
+   SHAPE2,
+   SHAPE3,
+   SHAPE_COUNT
+};
+
 class LineOptimizationTest : public Test
 {
 public:
@@ -15,17 +24,21 @@ public:
    virtual void init();
    virtual void deinit();
    virtual void run();
+   virtual void onKeyboardEvent(KeyboardEventType eventType, int keyCode) override;
 
 private:
+   void updateLines();
    tPolyLine createRandomPolyLine();
    void createPolyLineVBO(GLuint& polyLineVBO, const tPolyLine& polyLine);
    tPolyLine optimizePolyLine(const tPolyLine& polyLine, float threshold);
    void renderPolyLine(const GLuint polyLineVBO, const Vector4f& color, int count, float pointSize);
 
 private:
-   GLuint mPolyLineVBO;
-   GLuint mPolyLineOptimizedVBO;
+   GLuint mPolyLineVBO = 0;
+   GLuint mPolyLineOptimizedVBO = 0;
    tPolyLine mPolyLine;
    tPolyLine mPolyLineOptimized;
    PolyLineShader mPolyLineShader;
+   float mThreshold = 0.2f;
+   int mShape = SHAPE0;
 };
